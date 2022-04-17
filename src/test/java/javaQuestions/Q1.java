ab33a5e12022d0cc8bdb5c34fd3ac8de08d64d91
katefettah <katefettah@gmail.com>
2022-04-17 18:50:45 -0400

    v01

--- src/test/java/javaQuestions/Q1.java
+++ src/test/java/javaQuestions/Q1.java
@@ -1,5 +1,22 @@
 package javaQuestions;
 
+import java.util.HashMap;
+
 public class Q1 {
-    //1.line
+    public static void main(String[] args) {
+        String str = "This is a string. This program is counting words in a string. ";
+        String [] split= str.split(" ");
+
+        HashMap<String, Integer> map=new HashMap<>();
+        for(int i=0; i<split.length; i++){
+            if(map.containsKey(split[i])){
+                int count=map.get(split[i]);
+                map.put(split[i],count+1);
+            }
+            else {
+                map.put(split[i],1);
+            }
+        }
+        System.out.println(map);
+    }
 }
